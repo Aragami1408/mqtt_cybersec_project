986a2250b5e259fffefd2e22c2461a2757c59bf7
Aragami1408 <vucaominh1408@gmail.com>
2024-10-17 16:08:39 +1100

    [pass] publish to one target only

--- pass/pub.py
+++ pass/pub.py
@@ -2,20 +2,19 @@ from paho.mqtt import client as mqtt_client
 import random
 import logging
 import time
-import threading
 
-# broker = 'rule28.i4t.swin.edu.au'
-broker = "broker.emqx.io"
-port = 1883
-client_id = f"python-mqtt-{random.randint(0, 1000)}"
-#username = "<103825154>"
-#password = "<103825154>"
+# BROKER CONNECTION INFORMATION
+broker = 'rule28.i4t.swin.edu.au' # Server name
+port = 1883 # port number
+client_id = f"python-mqtt-{random.randint(0, 1000)}" # Client id
+username = "<103825154>" # Username
+password = "<103825154>" # Password
 
+# Reconnection constants
 FIRST_RECONNECT_DELAY = 1
 RECONNECT_RATE = 2
 MAX_RECONNECT_COUNT = 12
 MAX_RECONNECT_DELAY = 60
-
 
 # When the client initiate connection
 def on_connect(client, userdata, flags, rc):
@@ -24,7 +23,10 @@ def on_connect(client, userdata, flags, rc):
 	else:
 		print("Failed to connect, return code %d\n", rc)
 
-# When the client signify end of connection
+# When the client signifies end of connection
+# Auto reconnect upon disconnect for MAX_RECONNECT_COUNT times
+# each reconnect count will attempt to reconnect in FIRST_RECONNECT_DELAY
+# then reconnect delay will double after each count
 def on_disconnect(client, userdata, rc):
 	logging.info("Disconnected with result code: %s", rc)
 	reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
@@ -48,17 +50,16 @@ def on_disconnect(client, userdata, rc):
 # use this function to initiate connection
 def connect_mqtt():
 	client = mqtt_client.Client(client_id)
-	#client.username_pw_set(username, password)
+	client.username_pw_set(username, password)
 	client.on_connect = on_connect
 	client.on_disconnect = on_disconnect
 	client.connect(broker, port)
 	return client
 
-# We have two topics for now
 topic_temperature = "<103825154>/temperature"
-topic_humidity = "<103825154>/humidity"
 
 # Dummy function for temperature topic
+# This function publish a temperature message with random value
 def publish_temperature(client, topic_name):
 	while True:
 		try:
@@ -75,29 +76,11 @@ def publish_temperature(client, topic_name):
 			print("Terminate program")
 			break
 
-# Dummy function for humidity topic
-def publish_humidity(client, topic_name):
-	while True:
-		try:
-			time.sleep(2)
-			humid = random.randint(0,100)
-			msg = f"humidity: {humid}%"
-			result = client.publish(topic_name, msg)
-			status = result[0]
-			if status == 0:
-				print(f"Send `{msg}` to topic `{topic_name}`")
-			else:
-				print(f"Failed to send message to topic {topic_name}")
-		except KeyboardInterrupt:
-			print("Terminate program")
-			break
-
-
 if __name__ == "__main__":
+	# 1. Initiate connection
 	client = connect_mqtt()
 	client.loop_start()
 
-	threading.Thread(target=publish_temperature, args=(client, topic_temperature)).start()
-	threading.Thread(target=publish_humidity, args=(client, topic_humidity)).start()
+	publish_temperature(client, topic_temperature)
 
 	client.loop_stop()
--- pass/sub.py
+++ pass/sub.py
@@ -3,7 +3,6 @@ import random
 from paho.mqtt import client as mqtt_client
 
 broker = 'rule28.i4t.swin.edu.au'
-# broker = "broker.emqx.io"
 port = 1883
 # Generate a client id with prefix and random number
 client_id = f"python-mqtt-{random.randint(0, 1000)}"
